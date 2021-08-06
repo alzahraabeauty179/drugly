9566d60f13e53ac71c28d2df895563a949fcc78e
Ahmed Ali <Ahmedfci2018@gmail.com>
2021-08-06 13:52:54 +0200

    add products table and views

--- database/migrations/2021_03_01_074238_create_products_table.php
+++ database/migrations/2021_03_01_074238_create_products_table.php
@@ -16,11 +16,20 @@ class CreateProductsTable extends Migration
         Schema::create('products', function (Blueprint $table) {
             $table->increments('id');
 
-            // $table->integer('owner_id')->unsigned();
-            // $table->foreign('owner_id')->references('id')->on('users')->onDelete('cascade');
+            $table->integer('owner_id')->unsigned();
+            $table->foreign('owner_id')->references('id')->on('users')->onDelete('cascade');
 
-            $table->integer('category_id')->nullable()->unsigned();
+            $table->integer('category_id')->unsigned();
             $table->foreign('category_id')->references('id')->on('categories')->onDelete('cascade');
+
+            $table->integer('brand_id')->unsigned();
+            $table->foreign('brand_id')->references('id')->on('brands')->onDelete('cascade');
+
+            $table->double('amount');
+            $table->double('unit_price');
+
+            // $table->integer('parent_id')->nullable()->unsigned();
+            // $table->foreign('parent_id')->references('id')->on('products')->onDelete('cascade');
 
             $table->tinyInteger('active')->default(0);
             $table->string('image')->nullable();
