2b30d891dc51cfce8b86e4e343184729e6d37e4c
Alzahraa <alzahraa.ahmed179@gmail.com>
2021-10-11 19:56:36 +0200

    -Enhancement the solution of advertisements 'type is empty' issue.

--- resources/views/components/advertisements.blade.php
+++ resources/views/components/advertisements.blade.php
@@ -9,14 +9,8 @@
 
 @if( auth()->user()->type != "super_admin" )
     <div class="e3lan-container">
-        @if(count($h_ads)) 
-            <div class="horizontal"><img class="img-fluid" @php if(count($h_ads) != 0){ $h_ad = App\Models\Advertisement::where('id',$h_ads[array_rand($h_ads)])->first(); echo 'src="'. asset($h_ad->image_path) .'"';} @endphp /></div>
-        @endif
-        @if(count($v_ads))
-            <div class="vertical-right"><img class="img-fluid" @php if(count($v_ads) != 0){ $v_ad = App\Models\Advertisement::where('id',$v_ads[array_rand($v_ads)])->first(); echo 'src="'. asset($v_ad->image_path) .'"';} @endphp /></div>
-        @endif
-        @if(count($l_ads))
-            <div class="vertical-left"><img class="img-fluid" @php if(count($l_ads) != 0){ $l_ad = App\Models\Advertisement::where('id',$l_ads[array_rand($l_ads)])->first(); echo 'src="'. asset($l_ad->image_path) .'"';} @endphp /></div>
-        @endif
+        <div class="horizontal"><img class="img-fluid" @php if(count($h_ads) != 0){ $h_ad = App\Models\Advertisement::where('id',$h_ads[array_rand($h_ads)])->first(); echo 'src="'. asset($h_ad->image_path) .'"';} @endphp /></div>
+        <div class="vertical-right"><img class="img-fluid" @php if(count($v_ads) != 0){ $v_ad = App\Models\Advertisement::where('id',$v_ads[array_rand($v_ads)])->first(); echo 'src="'. asset($v_ad->image_path) .'"';} @endphp /></div>
+        <div class="vertical-left"><img class="img-fluid" @php if(count($l_ads) != 0){ $l_ad = App\Models\Advertisement::where('id',$l_ads[array_rand($l_ads)])->first(); echo 'src="'. asset($l_ad->image_path) .'"';} @endphp /></div>
     </div>
 @endif
