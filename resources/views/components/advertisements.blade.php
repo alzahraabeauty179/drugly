ecb16d84772493b551e4c97cef5bdb72774d67e7
Alzahraa <alzahraa.ahmed179@gmail.com>
2021-10-11 17:14:30 +0200

    -Solved advertisements type is empty issue.

--- resources/views/components/advertisements.blade.php
+++ resources/views/components/advertisements.blade.php
@@ -5,22 +5,18 @@
         ->where('active',1)->where('display_method', 'vertical')->pluck('id')->toArray();
     $l_ads = App\Models\Advertisement::where('end_date','>=',Carbon\Carbon::now())
         ->where('active',1)->where('display_method', 'longitudinal')->pluck('id')->toArray();
-
-    $h_ad = App\Models\Advertisement::where('id',$h_ads[array_rand($h_ads)])->first();
-    $v_ad = App\Models\Advertisement::where('id',$v_ads[array_rand($v_ads)])->first();
-    $l_ad = App\Models\Advertisement::where('id',$l_ads[array_rand($l_ads)])->first();
 @endphp
 
 @if( auth()->user()->type != "super_admin" )
     <div class="e3lan-container">
-        @if(count($h_ads))
-            <div class="horizontal"><img class="img-fluid" src="{{ asset($h_ad->image_path) }}" /></div>
+        @if(count($h_ads)) 
+            <div class="horizontal"><img class="img-fluid" @php if(count($h_ads) != 0){ $h_ad = App\Models\Advertisement::where('id',$h_ads[array_rand($h_ads)])->first(); echo 'src="'. asset($h_ad->image_path) .'"';} @endphp /></div>
         @endif
         @if(count($v_ads))
-            <div class="vertical-right"><img class="img-fluid" src="{{ asset($v_ad->image_path) }}" /></div>
+            <div class="vertical-right"><img class="img-fluid" @php if(count($v_ads) != 0){ $v_ad = App\Models\Advertisement::where('id',$v_ads[array_rand($v_ads)])->first(); echo 'src="'. asset($v_ad->image_path) .'"';} @endphp /></div>
         @endif
         @if(count($l_ads))
-            <div class="vertical-left"><img class="img-fluid" src="{{ asset($l_ad->image_path) }}" /></div>
+            <div class="vertical-left"><img class="img-fluid" @php if(count($l_ads) != 0){ $l_ad = App\Models\Advertisement::where('id',$l_ads[array_rand($l_ads)])->first(); echo 'src="'. asset($l_ad->image_path) .'"';} @endphp /></div>
         @endif
     </div>
 @endif
